0861e2676465ce7eefd17cbfbbfb650cdc0ff8fa
asa.stallworth <asa.stallworth@gmail.com>
2018-12-25 17:47:17 -0500

    First Commit

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php 
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $formcontent="From: $name \n Message: $message";
+    $recipent ="asa@asastallworth.com";
+    $subject ="Contact Form";
+    $mailheader ="From: $email \r\n;";
+
+    mail($recipent, $subject, $formcontent, $mailheader)
+     or die("Error!");
+
+    echo "Thank You!";
+
+
+?>
